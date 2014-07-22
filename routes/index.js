b0eb80c2c0b80dbe561cbe4e8c9ad85ef4e9685b
aleppington <andy.leppington@quantiv.com>
2014-07-22 16:45:11 +0100

    Rename title in index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'My Express App' });
 });
 
-module.exports = router;sss
+module.exports = router;
